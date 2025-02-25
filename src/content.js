b9fc3a9c41ecaf6a122dd2dd817fe2f8229fa421
Elisha Jeanbaptiste <113145351+ChaosShadoww@users.noreply.github.com>
2025-02-24 19:52:35 -0500

    starting build

---
+++ src/content.js
@@ -0,0 +1,6 @@
+import * as InboxSDK from '@inboxsdk/core';
+InboxSDK.load(2, "App_id").then((sdk) => {
+  
+});
+// sTEp 4.1
+//https://medium.com/@intuitionlabs/creating-a-powerful-ai-powered-chrome-extension-a-step-by-step-guide-b0a200955469
